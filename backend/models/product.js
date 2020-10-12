8cfa051a0464249fd6494c8887aa190ebadbca2a
nrednav <nrednav@gmail.com>
2020-10-12 19:18:30 +0400

    Create db seeder script

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
-const reviewSchema = mongoose.Schema({
-  name: { type: String, required: true},
-  rating: { type: Number, required: true},
-  comment: { type: String, required: true},
-}, {
-  timestamps: true
-});
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const productSchema = mongoose.Schema(
   {
@@ -35,7 +38,7 @@ const productSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    reviews: [reviewSchema]
+    reviews: [reviewSchema],
     rating: {
       type: Number,
       required: true,
